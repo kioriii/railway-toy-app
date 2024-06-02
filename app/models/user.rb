bc4f5f547cb90f97a5ebd8598e4a7fd86854a542
kioriii <anguskoh@gmail.com>
2024-06-02 17:11:54 +0800

    Added has_many and belongs_to to associate Users model with Microposts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
 end
 
 # it's a rather plain doc here, but it's because the scaffold generates this model and utilizes the inheritance from the Rails library "Active Record"
