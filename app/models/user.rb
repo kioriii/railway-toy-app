69b8f5db3f85d913ee2cad7a21442fee0898eb61
kioriii <anguskoh@gmail.com>
2024-06-03 00:07:37 +0800

    Added validation to the User model to ensure name and email are present. Do note the syntax needed, the colon is important!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
 
 # it's a rather plain doc here, but it's because the scaffold generates this model and utilizes the inheritance from the Rails library "Active Record"
